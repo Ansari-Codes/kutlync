b464230829f0e72498440730a9540a722f77361b
Ansari-Codes <ansaricodes@gmail.com>
2026-09-09 14:54:43 +0500

    .

--- frontend/src/Components/Main/CompMainHeader.vue
+++ frontend/src/Components/Main/CompMainHeader.vue
@@ -10,12 +10,13 @@ import KLink from '@W/Actions/KLink.vue'
             <Logo :width="36" :height="36" :text-size="22" />
         </KLink>
 
-        <nav class="hidden items-center gap-3 sm:flex" aria-label="Main navigation">
-            <KLink href="https://github.com/Ansari-Codes/kutlync" target="_blank" rel="noopener noreferrer">
-                GitHub
-            </KLink>
-            <KButton label="Start shortening" @click="$router.push('/auth/signup')" />
-
+        <nav class="items-center gap-3 flex flex-row " aria-label="Main navigation">
+            <div class="w-fit gap-3 hidden sm:flex">
+                <KLink href="https://github.com/Ansari-Codes/kutlync" target="_blank" rel="noopener noreferrer">
+                    GitHub
+                </KLink>
+                <KButton label="Start shortening" @click="$router.push('/auth/signup')" />
+            </div>
             <KLink to="/auth/login" class="sm:hidden no-underline hover:no-underline">
                 <KButton label="Login" />
             </KLink>
